04a88402432c0ac2361b6c283ace52676967e65f
ArnabTo <arnabs929@gmail.com>
2023-12-23 23:08:44 +0600

    created navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,46 +1,34 @@
 "use client";
 import { Button, Navbar } from "keep-react";
-import { MagnifyingGlass } from "phosphor-react";
+import './Nav.css'
+
 const Nav = () => {
     return (
         <div className="max-w-6xl mx-auto">
             <Navbar fluid={true} className='bg-transparent' >
-                <Navbar.Container className="flex items-center justify-between">
+                <Navbar.Container className="flex items-center justify-center">
                     <Navbar.Container
                         tag="ul"
-                        className="lg:flex hidden items-center justify-between gap-8"
+                        className="lg:flex hidden items-center justify-between gap-8 text-white"
                     >
                         <Navbar.Link linkName="Home" />
                         <Navbar.Link linkName="Projects" />
+                        <Navbar.Brand>
+                            Logo
+                        </Navbar.Brand>
+                        <Navbar.Link linkName="Projects" />
                         <Navbar.Link linkName="Blogs" />
                     </Navbar.Container>
-                    <Navbar.Brand>
-                        jhslkdfj
-                    </Navbar.Brand>
-                    <Navbar.Collapse collapseType="sidebar">
-                        <Navbar.Container tag="ul" className="flex flex-col gap-5">
-                            <Navbar.Link  linkName="Home" />
+                    <Navbar.Collapse collapseType="sidebar" className="bg-black">
+                        <Navbar.Container tag="ul" className="flex flex-col gap-5 text-white">
+                            <Navbar.Link linkName="Home" />
                             <Navbar.Link linkName="Projects" />
                             <Navbar.Link linkName="Blogs" />
                             <Navbar.Link linkName="News" />
                             <Navbar.Link linkName="Resources" />
                         </Navbar.Container>
                     </Navbar.Collapse>
-
                     <Navbar.Container className="flex items-center gap-3">
-                        <Navbar.Container
-                            tag="ul"
-                            className="lg:flex hidden items-center justify-between gap-5"
-                        >
-                            <Navbar.Link linkName="News" />
-                            <Navbar.Link linkName="Resources" />
-                        </Navbar.Container>
-                        <Button size="sm" type="link">
-                            <span>
-                                <MagnifyingGlass size={20} color="#444" />
-                            </span>
-                            <span className="ml-2 text-metal-600">Search</span>
-                        </Button>
                         <Navbar.Toggle />
                     </Navbar.Container>
                 </Navbar.Container>
