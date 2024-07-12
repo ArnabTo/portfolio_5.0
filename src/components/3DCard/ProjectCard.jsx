187af5ed6d7719e980aa51dbd88a5fc7034b9862
ArnabTo <arnabs929@gmail.com>
2024-07-12 19:32:57 +0600

    updated ui

--- src/components/3DCard/ProjectCard.jsx
+++ src/components/3DCard/ProjectCard.jsx
@@ -7,17 +7,17 @@ const ProjectCard = ({ proj }) => {
 
     return (
         <CardContainer className="inter-var">
-            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl  dark:hover:shadow-[#8F00FF]/[0.3] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full lg:w-[30rem] xl:w-[35rem] h-auto rounded-xl p-6 border">
+            <CardBody className=" bg-zinc-950 relative group/card  dark:hover:shadow-2xl  dark:hover:shadow-[#8F00FF]/[0.3] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full lg:w-[30rem] xl:w-[35rem] h-auto rounded-xl p-6 border">
                 <CardItem
                     translateZ="50"
-                    className="text-xl font-bold text-black dark:text-white"
+                    className="text-xl font-bold text-white dark:text-white"
                 >
                     {proj?.title}
                 </CardItem>
                 <CardItem
                     as="p"
                     translateZ="60"
-                    className="text-black text-sm max-w-sm mt-2 dark:text-neutral-300"
+                    className="text-white text-sm max-w-sm mt-2 dark:text-neutral-300"
                 >
                     Production grade fitness tracking application. Built with a team of 6 developers.
                 </CardItem>
@@ -45,15 +45,14 @@ const ProjectCard = ({ proj }) => {
                         as={Link}
                         href={proj?.github}
                         target="__blank"
-                        className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                        className="text-white px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                     >
-
                         Repo →
                     </CardItem>
                     <CardItem
                         translateZ={20}
                         as="button"
-                        className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                        className="px-4 py-2 rounded-xl bg-white dark:bg-black dark:text-white text-black text-xs font-bold"
                     >
                         <Link href={proj?.live}>Preview</Link>
                     </CardItem>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,7 +9,9 @@ const Projects = async () => {
 
     return (
         <div className="max-w-7xl mx-4 lg:mx-auto my-24" id='projs'>
-            <h2 className="text-5xl text-[#8F00FF] text-start">Latest Projects.</h2>
+            <h1 className="text-center lg:text-start text-3xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-purple-200 to-purple-600 my-4">
+                Latest Projects
+            </h1>
             <div className='grid grid-cols-1 lg:grid-cols-2'>
                 <div>
                     <ProjectCard proj={projects[0]} />
@@ -24,22 +26,6 @@ const Projects = async () => {
                     <ProjectCard proj={projects[3]} />
                 </div>
             </div>
-            {/* <div className="grid
-                      grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-10
-                      grid-rows-4 md:grid-rows-5 lg:grid-rows-11">
-                <div className="col-span-1 sm:col-span-2 md:col-span-4 lg:col-span-5 row-span-2 md:row-span-2 lg:row-span-5  p-4">
-                    <Project proj={projects[0]}></Project>
-                </div>
-                <div className="col-span-1 sm:col-span-2 md:col-span-4 lg:col-span-5 row-span-2 md:row-span-2 lg:row-span-5 col-start-1 sm:col-start-2 md:col-start-3 lg:col-start-6 row-start-1 sm:row-start-1 md:row-start-2 lg:row-start-2  p-4">
-                    <Project proj={projects[1]}></Project>
-                </div>
-                <div className="col-span-1 sm:col-span-2 md:col-span-4 lg:col-span-5 row-span-2 md:row-span-2 lg:row-span-5  p-4">
-                    <Project proj={projects[2]}></Project>
-                </div>
-                <div className="col-span-1 sm:col-span-2 md:col-span-4 lg:col-span-5 row-span-2 md:row-span-2 lg:row-span-5 col-start-1 sm:col-start-2 md:col-start-3 lg:col-start-6 row-start-1 sm:row-start-2 md:row-start-3 lg:row-start-7  p-4">
-                    <Project proj={projects[3]}></Project>
-                </div>
-            </div> */}
             <SeeMBtn />
         </div>
     );
