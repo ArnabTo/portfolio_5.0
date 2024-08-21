cc293a72fd221694d5e5ef45d90f63be6a74f16b
ArnabTo <arnabs929@gmail.com>
2024-08-21 21:05:59 +0600

    updated api

--- src/utils/getAllProjects.js
+++ src/utils/getAllProjects.js
@@ -1,7 +1,10 @@
 import axios from "axios"
 
 export const getAllProjects = async () => {
-    const response = await axios.get('http://localhost:3000/api/get-projects');
-   console.log(response.data)
+    const response = await axios.get('https://arsdevs.vercel.app/api/get-projects');
+
+    if(response.data.success === false) {
+        throw new Error(response.data.message);
+    }
     return response.data;
 }
--- src/utils/getLatestProjects.js
+++ src/utils/getLatestProjects.js
@@ -1,7 +1,10 @@
 import axios from "axios"
 
 export const getLatesstProjects = async () => {
-    const response = await axios.get('http://localhost:3000/api/get-latest-projects');
- console.log(response.data)
+    const response = await axios.get('https://arsdevs.vercel.app/api/get-latest-projects');
+    
+    if(response.data.success === false) {
+        throw new Error(response.data.message);
+    }
     return response.data;
 }
