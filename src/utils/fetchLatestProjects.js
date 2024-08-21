d6efea31646b8d4008fe45b0e5f12fdffbd0581a
ArnabTo <arnabs929@gmail.com>
2024-08-21 23:18:15 +0600

    updated

--- src/utils/fetchLatestProjects.js
+++ src/utils/fetchLatestProjects.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
 export default async function fetchProjects() {
+
+    const baseUrl = process.env.NODE_ENV === 'production' 
+    ? 'https://arsdevs.vercel.app' 
+    : 'http://localhost:3000';
+
+    console.log(baseUrl)
+
     try {
-        const response = await axios.get('/api/get-latest-projects');
+        const response = await axios.get(`${baseUrl}/api/get-latest-projects`);
         console.log(response.data)
         if (response.data.success) {
             return response.data.data;
