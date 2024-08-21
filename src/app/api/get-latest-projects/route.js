d841a020fe19d32791acfdb7dc602efc8054327a
ArnabTo <arnabs929@gmail.com>
2024-08-21 19:55:01 +0600

    created latest project api

---
+++ src/app/api/get-latest-projects/route.js
@@ -0,0 +1,28 @@
+import connectDB from "@/lib/connectDB";
+import Project, { create } from "@/model/ProjectModel";
+
+export async function GET() {
+    await connectDB();
+
+    try {
+        const projects = await Project.find().sort({ createdAt: -1 }).limit(4);
+
+        if (!projects || projects.length === 0) {
+            return new Response(JSON.stringify({
+                success: false,
+                message: 'No projects found'
+            }), { status: 404 })
+        }
+        return new Response(JSON.stringify({
+            success: true,
+            message: 'Latest projects fetched successfully',
+            data: projects
+        }), { status: 200 })
+    } catch (error) {
+        console.log(error);
+        return new Response(JSON.stringify({
+            success: false,
+            message: error.message
+        }), { status: 500 })
+    }
+}
