8c72fb647c2837767203de9234e3bbbcd9926d01
ArnabTo <arnabs929@gmail.com>
2024-08-21 22:13:36 +0600

    updated laoder

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,15 +1,17 @@
 'use client';
 import { Bars } from 'react-loader-spinner';
 const loading = () => {
-    return <Bars
-    height="85"
-    width="85"
-    color="#8F00FF"
-    ariaLabel="bars-loading"
-    wrapperStyle={{}}
-    wrapperClass=""
-    visible={true}
-    />;
+    return <div className='h-screen flex justify-center items-center'>
+        <Bars
+            height="60"
+            width="60"
+            color="#8F00FF"
+            ariaLabel="bars-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+        />
+    </div>;
 };
 
 export default loading;
--- src/utils/getAllProjects.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-export const getAllProjects = async () => {
-    const response = await axios.get('/api/get-projects');
-    console.log(response.data);
-    return response.data;
-}
