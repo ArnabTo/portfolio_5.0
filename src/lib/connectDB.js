f028efc6400d091e00b192c8c531ad4008ccf7bc
ArnabTo <arnabs929@gmail.com>
2024-08-22 19:45:24 +0600

    updated database connection

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 
 export default async function connectDB() {
-    
+
     try {
-        const db = mongoose.connect(process.env.DB_URI);
-        // console.log(db)
+        const db = await mongoose.connect(process.env.DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('Database connected 🎊');
     } catch (error) {
         console.log(error);
